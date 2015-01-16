a8e5f21ac8168a8720052121deaaa7ec02fec6b5
nChaufournier <nchaufournier140@cardinal.wju.edu>
2015-01-15 22:51:39 -0500

    Started implementing a database. Also added all of the event classed to add the the schedule

--- app/src/main/java/com/sleepypirate/athletemanager/schedule/event.java
+++ app/src/main/java/com/sleepypirate/athletemanager/schedule/event.java
@@ -1,7 +1,41 @@
 package com.sleepypirate.athletemanager.schedule;
 
+import java.util.Date;
+
 /**
- * This event will be a new thing added to the schedule. Such as a class or a homework assignment
+ * Created by Nic on 1/15/2015.
  */
-public class event {
+public class Event {
+
+    String name;
+    String note;
+    Date dueDate;
+
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
 }
